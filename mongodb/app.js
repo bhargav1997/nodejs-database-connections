5460e33eb0193aebebca8abadab983d28ce65740
Bhargav Suthar <sutharbhargav1997@gmail.com>
2024-03-10 15:46:52 -0400

    Update app.js

--- mongodb/app.js
+++ mongodb/app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
 const db = require('./db');
+require('./auth');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({ url: process.env.MONGODB_URL })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 db.connect().then((database) => {
   // Now you can use the `database` object to interact with your database
-
+  // Routes --Authentication
+  app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }), function(req, res) {
+    res.redirect('/');
+  });
+  
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+  
   // GET request
   app.get('/data', (req, res) => {
     database.collection('data').find({}).toArray((err, result) => {
